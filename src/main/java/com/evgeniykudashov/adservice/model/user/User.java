1defd1dfd5cb5b950d96facdadc4c33a35dc8dca
Eugene <stampede147@gmail.com>
2023-04-15 15:55:05 +0300

    updated User.java implementation

--- src/main/java/com/evgeniykudashov/adservice/model/user/User.java
+++ src/main/java/com/evgeniykudashov/adservice/model/user/User.java
@@ -1,7 +1,18 @@
 package com.evgeniykudashov.adservice.model.user;
 
 
+import com.evgeniykudashov.adservice.model.user.valueobject.Fullname;
+import com.evgeniykudashov.adservice.model.user.valueobject.UserSecretDetails;
+import jakarta.persistence.Column;
 import lombok.Getter;
 
 public class User {
+
+    @Column(name = "user_id")
+    private Long id;
+
+    private Fullname fullname;
+    private UserSecretDetails userSecretDetails;
+
+
 }
