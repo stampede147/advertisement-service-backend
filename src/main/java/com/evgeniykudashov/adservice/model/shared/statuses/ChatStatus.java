abfc5da13a51fe4939114bd76905c77f57559b7d
Eugene <stampede147@gmail.com>
2023-04-15 18:34:26 +0300

    deleted

---
+++ src/main/java/com/evgeniykudashov/adservice/model/shared/statuses/ChatStatus.java
@@ -0,0 +1,6 @@
+package com.evgeniykudashov.adservice.model.shared.statuses;
+
+public enum ChatStatus {
+
+    CREATED, DELETED
+}
---
+++ src/main/java/com/evgeniykudashov/adservice/model/shared/statuses/MessageStatus.java
@@ -0,0 +1,8 @@
+package com.evgeniykudashov.adservice.model.shared.statuses;
+
+public enum MessageStatus {
+
+    CREATED,
+    UPDATED,
+    DELETED
+}
