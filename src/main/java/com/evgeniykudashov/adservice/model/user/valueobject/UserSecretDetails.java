b383ccc4b7cccba88024ce306499cabfd726d9f4
Eugene <stampede147@gmail.com>
2023-04-15 16:12:25 +0300

    added no args and full args constructors in UserSecretDetails.java

--- src/main/java/com/evgeniykudashov/adservice/model/user/valueobject/UserSecretDetails.java
+++ src/main/java/com/evgeniykudashov/adservice/model/user/valueobject/UserSecretDetails.java
@@ -15,11 +15,20 @@ import java.util.Collection;
 @Embeddable
 public class UserSecretDetails implements UserDetails {
 
-
     @Column(name = "username")
     private String username;
     @Column(name = "password")
     private String password;
+
+
+    @Deprecated
+    public UserSecretDetails() {
+    }
+
+    public UserSecretDetails(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
