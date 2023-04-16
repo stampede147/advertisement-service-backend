14160904ece9c1a6588662fa5f0cc25f3dfa82e6
Eugene <stampede147@gmail.com>
2023-04-16 14:34:30 +0300

    added CreationDateTime.java

---
+++ src/main/java/com/evgeniykudashov/adservice/model/shared/CreationDateTime.java
@@ -0,0 +1,9 @@
+package com.evgeniykudashov.adservice.model.shared;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class CreationDateTime {
+
+    private LocalDateTime localDateTime;
+}
