42e29726546ffcbee2f3c9410fd14551b38adf1a
Eugene <stampede147@gmail.com>
2023-04-16 14:34:58 +0300

    added MessageBody.java

---
+++ src/main/java/com/evgeniykudashov/adservice/model/chat/valueobject/MessageBody.java
@@ -0,0 +1,10 @@
+package com.evgeniykudashov.adservice.model.chat.valueobject;
+
+import java.io.Serializable;
+
+public class MessageBody implements Serializable {
+
+    private String data;
+
+
+}
