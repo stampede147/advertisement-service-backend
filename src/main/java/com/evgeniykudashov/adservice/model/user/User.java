f35d3a3ca268641bb809bb905c2f6bd788999320
Eugene <stampede147@gmail.com>
2023-04-16 11:39:04 +0300

    User.java updated implementation

--- src/main/java/com/evgeniykudashov/adservice/model/user/User.java
+++ src/main/java/com/evgeniykudashov/adservice/model/user/User.java
@@ -1,11 +1,12 @@
 package com.evgeniykudashov.adservice.model.user;
 
 
+import com.evgeniykudashov.adservice.model.shared.ActivityDateTime;
 import com.evgeniykudashov.adservice.model.user.valueobject.Fullname;
-import com.evgeniykudashov.adservice.model.user.valueobject.OnlineTime;
 import com.evgeniykudashov.adservice.model.user.valueobject.UserSecretDetails;
 import jakarta.persistence.Column;
-import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 public class User {
 
@@ -13,8 +14,28 @@ public class User {
     private Long id;
 
     private Fullname fullname;
-    private UserSecretDetails userSecretDetails;
-    private OnlineTime lastOnlineTime;
+    private UserSecretDetails secretDetails;
+    private ActivityDateTime activityDateTime;
+
+    public User() {
+    }
+
+    public User(Fullname fullname,
+                UserSecretDetails secretDetails, LocalDateTime creationDateTime) {
+        this.fullname = fullname;
+        this.secretDetails = secretDetails;
+        this.activityDateTime = ActivityDateTime.startFrom(creationDateTime);
+    }
+
+    public void changeFullName(Fullname fullname, LocalDateTime changeDateTime) {
+        this.fullname = fullname;
+        this.activityDateTime = activityDateTime.withLastUpdateDateTime(changeDateTime);
+    }
+
+    public void changeSecretDetails(UserSecretDetails secretDetails, LocalDateTime changeDateTime) {
+        this.secretDetails = secretDetails;
+        this.activityDateTime = activityDateTime.withLastUpdateDateTime(changeDateTime);
+    }
 
 
 }
