54a8e76c2df35f0b5ae0b97d4303d2d91e0b89a5
Eugene <stampede147@gmail.com>
2023-04-29 19:30:11 +0300

    added new value object, constructor

--- src/main/java/com/evgeniykudashov/adservice/model/domain/aggregate/user/User.java
+++ src/main/java/com/evgeniykudashov/adservice/model/domain/aggregate/user/User.java
@@ -1,7 +1,8 @@
 package com.evgeniykudashov.adservice.model.domain.aggregate.user;
 
 
-import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.Fullname;
+import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.AccessDetails;
+import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.PersonalDetails;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,13 +19,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
-    private Fullname fullname;
 
-    public User(Fullname fullname) {
-        this.fullname = fullname;
+    private PersonalDetails personalDetails;
+
+    private AccessDetails accessDetails;
+
+    public User(PersonalDetails personalDetails, AccessDetails accessDetails) {
+        this.personalDetails = personalDetails;
+        this.accessDetails = accessDetails;
     }
 
-    public void changeFullName(Fullname fullname) {
-        this.fullname = fullname;
+    public void changePersonalDetails(PersonalDetails personalDetails) {
+        this.personalDetails = personalDetails;
+    }
+
+    public void changeAccessDetails(AccessDetails accessDetails) {
+        this.accessDetails = accessDetails;
     }
 }
