f367286c08fc843375217d721b9d70b6cef086d4
Eugene <stampede147@gmail.com>
2023-04-29 14:50:28 +0300

    added CategoryRepository.java

---
+++ src/main/java/com/evgeniykudashov/adservice/repository/categoryrepository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.evgeniykudashov.adservice.repository.categoryrepository;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface CategoryRepository extends ListCrudRepository<CategoryRepository, Long> {
+}
