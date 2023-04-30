778d616b845e2c16343807e338265412821f006d
Eugene <stampede147@gmail.com>
2023-04-30 14:20:37 +0300

    added repositories
    added repositories
    
    deleted

--- src/main/java/com/evgeniykudashov/adservice/repository/categoryrepository/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.evgeniykudashov.adservice.repository.categoryrepository;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface CategoryRepository extends ListCrudRepository<CategoryRepository, Long> {
-}
