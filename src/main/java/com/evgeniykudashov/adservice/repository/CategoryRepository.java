d4131ad4e7cf269fc88a3b66432107c00988aa4b
Eugene <stampede147@gmail.com>
2023-05-02 10:19:33 +0300

    added findRootCategories method

--- src/main/java/com/evgeniykudashov/adservice/repository/CategoryRepository.java
+++ src/main/java/com/evgeniykudashov/adservice/repository/CategoryRepository.java
@@ -1,7 +1,14 @@
 package com.evgeniykudashov.adservice.repository;
 
 import com.evgeniykudashov.adservice.model.domain.aggregate.category.Category;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends ListCrudRepository<Category, Long> {
+
+    @Query("select c from Category c where c.parent = null")
+    public List<Category> findRootCategories();
+
 }
