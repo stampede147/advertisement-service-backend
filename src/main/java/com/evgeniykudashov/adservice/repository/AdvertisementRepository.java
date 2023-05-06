f83b5131252779c69c2bae42d896e8cccce013a5
Eugene <stampede147@gmail.com>
2023-05-06 18:16:26 +0300

    updated repositories interfaces

--- src/main/java/com/evgeniykudashov/adservice/repository/AdvertisementRepository.java
+++ src/main/java/com/evgeniykudashov/adservice/repository/AdvertisementRepository.java
@@ -1,7 +1,16 @@
 package com.evgeniykudashov.adservice.repository;
 
 import com.evgeniykudashov.adservice.model.domain.aggregate.advertisement.Advertisement;
+import com.evgeniykudashov.adservice.model.domain.shared.Title;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface AdvertisementRepository extends ListCrudRepository<Advertisement, Long> {
+
+    List<Advertisement> findAllByOwnerId(long ownerUserId);
+
+    List<Advertisement> findAllByCategoryId(long categoryId);
+
+    List<Advertisement> findAllByTitle(Title title);
 }
