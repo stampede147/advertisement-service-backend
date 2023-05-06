36427b914d0929d029cb803000a66a5cbbe4cbb3
Eugene <stampede147@gmail.com>
2023-05-06 18:16:26 +0300

    updated repositories interfaces

--- src/main/java/com/evgeniykudashov/adservice/repository/FeedbackRepository.java
+++ src/main/java/com/evgeniykudashov/adservice/repository/FeedbackRepository.java
@@ -3,5 +3,11 @@ package com.evgeniykudashov.adservice.repository;
 import com.evgeniykudashov.adservice.model.domain.aggregate.feedback.Feedback;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface FeedbackRepository extends ListCrudRepository<Feedback, Long> {
+
+    List<Feedback> findAllByRecipientId(Long recipientUserId);
+
+    List<Feedback> findAllBySenderId(Long senderUserId);
 }
