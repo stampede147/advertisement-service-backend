c85e2768c51ba7b8fabc508c891c4d8e6fb38fc5
Eugene <stampede147@gmail.com>
2023-06-12 20:55:18 +0300

    added new helper utility classes

---
+++ src/main/java/com/evgeniykudashov/adservice/controller/service/helper/StringUtils.java
@@ -0,0 +1,18 @@
+package com.evgeniykudashov.adservice.controller.service.helper;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Objects;
+
+@UtilityClass
+public class StringUtils {
+
+    public String capitalize(String str) {
+        Objects.requireNonNull(str);
+        return str.substring(0, 1).concat(str.substring(1));
+    }
+
+    public String concat(String str1, String str2) {
+        return str1.concat(str2);
+    }
+}
