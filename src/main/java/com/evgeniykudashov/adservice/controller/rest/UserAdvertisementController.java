f137adeb1c38555ee822ea660c405df89be52fbc
Eugene <stampede147@gmail.com>
2023-06-21 15:58:32 +0300

    added new endpoint

--- src/main/java/com/evgeniykudashov/adservice/controller/rest/UserAdvertisementController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/UserAdvertisementController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -78,6 +79,7 @@ public class UserAdvertisementController {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{id}")
     public ResponseEntity<?> onGetById(@PathVariable Long id) {
         return ResponseEntity.ok(advertisementService.findById(id));
