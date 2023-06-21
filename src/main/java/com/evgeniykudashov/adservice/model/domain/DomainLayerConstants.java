e0c2deb39f34ea4f34b0a045ae21c33d5d2b93cb
Eugene <stampede147@gmail.com>
2023-06-21 15:49:41 +0300

    added constants

--- src/main/java/com/evgeniykudashov/adservice/model/domain/DomainLayerConstants.java
+++ src/main/java/com/evgeniykudashov/adservice/model/domain/DomainLayerConstants.java
@@ -1,5 +1,9 @@
 package com.evgeniykudashov.adservice.model.domain;
 
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class DomainLayerConstants {
 
     public static final String UPDATE_METHOD_PREFIX = "update";
