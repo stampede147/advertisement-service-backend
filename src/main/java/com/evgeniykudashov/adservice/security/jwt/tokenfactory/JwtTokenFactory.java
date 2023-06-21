7feb3170e53a32a5da69fd0e89a40a40596ac843
Eugene <stampede147@gmail.com>
2023-06-21 16:02:15 +0300

    added ouath token factory

---
+++ src/main/java/com/evgeniykudashov/adservice/security/jwt/tokenfactory/JwtTokenFactory.java
@@ -0,0 +1,59 @@
+package com.evgeniykudashov.adservice.security.jwt.tokenfactory;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.Role;
+import com.evgeniykudashov.adservice.security.jwt.JwtAuthenticationToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+
+@Component("jwtTokenFactory")
+public class JwtTokenFactory implements OAuthTokenFactory {
+
+    protected String jwtSecret;
+
+    protected int tokenLifeMillis;
+
+    private Algorithm algorithm;
+
+    public JwtTokenFactory(@Value("${application.security.jwt.secret}") String jwtSecret,
+                           @Value("${application.security.jwt.lifeMillis}") int tokenLifeMillis) {
+        this.jwtSecret = jwtSecret;
+        this.tokenLifeMillis = tokenLifeMillis;
+        this.algorithm = Algorithm.HMAC256(jwtSecret.getBytes());
+    }
+
+    @Override
+    public String createToken(String username, Collection<? extends GrantedAuthority> authorities) {
+        return JWT.create()
+                .withClaim(JwtTokenConstants.sub, username)
+                .withArrayClaim(JwtTokenConstants.roles, authorities.stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .toArray(String[]::new))
+                .withClaim(JwtTokenConstants.issuedAt, Instant.now())
+                .withClaim(JwtTokenConstants.expiresAt, Instant.now().plusMillis(tokenLifeMillis))
+                .sign(algorithm);
+    }
+
+    @Override
+    public JwtAuthenticationToken validateAndDecodeToken(String token) {
+        DecodedJWT rawToken = validate(token);
+        return new JwtAuthenticationToken(
+                List.of(rawToken.getClaim(JwtTokenConstants.roles).asArray(Role.class)),
+                rawToken.getSubject(),
+                rawToken.getExpiresAtAsInstant(),
+                rawToken.getIssuedAtAsInstant());
+    }
+
+    private DecodedJWT validate(String token) {
+        return JWT.require(algorithm).build().
+                verify(token);
+    }
+
+}
---
+++ src/main/java/com/evgeniykudashov/adservice/security/jwt/tokenfactory/OAuthTokenFactory.java
@@ -0,0 +1,15 @@
+package com.evgeniykudashov.adservice.security.jwt.tokenfactory;
+
+import com.evgeniykudashov.adservice.security.jwt.exception.DecodeException;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public interface OAuthTokenFactory {
+
+    String createToken(String username, Collection<? extends GrantedAuthority> authorities);
+
+    AbstractAuthenticationToken validateAndDecodeToken(String token) throws DecodeException;
+
+}
