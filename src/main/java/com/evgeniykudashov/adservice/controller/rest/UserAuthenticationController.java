43013df7876eb0a8bf7f8e345339f2c7f88041dd
Eugene <stampede147@gmail.com>
2023-06-24 21:42:55 +0300

    changed mapping requests

--- src/main/java/com/evgeniykudashov/adservice/controller/rest/UserAuthenticationController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/UserAuthenticationController.java
@@ -1,29 +1,46 @@
 package com.evgeniykudashov.adservice.controller.rest;
 
 
-import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.AccessDetails;
+import com.evgeniykudashov.adservice.mapper.dto.authentication.CreateAuthenticationRequestDto;
 import com.evgeniykudashov.adservice.service.AuthenticationService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authentications")
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class UserAuthenticationController {
 
+    public static final String ACCESS_TOKEN = "access_token";
     protected AuthenticationService authenticationService;
 
+    private static Cookie getSecuredHttpOnlyCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        return cookie;
+    }
+
     @SneakyThrows
-    @PostMapping("/jwts")
-    public ResponseEntity<?> createAuthentication(@RequestBody AccessDetails accessDetails) {
-        return ResponseEntity
-                .ok(authenticationService.generateJwtToken(accessDetails));
+    @PostMapping("/jwt")
+    public ResponseEntity<Void> createJwtAuthentication(@RequestBody CreateAuthenticationRequestDto requestDto, HttpServletResponse httpResponse) {
+        String token = authenticationService.generateJwtToken(requestDto.getUsername(), requestDto.getPassword());
+
+        httpResponse.addCookie(getSecuredHttpOnlyCookie(ACCESS_TOKEN, token));
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> get(@AuthenticationPrincipal Object obj) {
+
+        return ResponseEntity.ok(obj.toString());
     }
 
 
