8c3852a9fb8046dd1c7ea967d5dd1bef2fa70e70
Eugene <stampede147@gmail.com>
2023-06-27 13:57:51 +0300

    changed to anemic model

---
+++ src/main/java/com/evgeniykudashov/adservice/model/domain/Role.java
@@ -0,0 +1,27 @@
+package com.evgeniykudashov.adservice.model.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+
+    USER(Roles.ROLE_USER), ADMIN(Roles.ROLE_ADMIN);
+
+    String authority;
+
+    Role(String val) {
+        this.authority = val;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public static class Roles {
+        public static final String USER = "USER";
+        public static final String ADMIN = "ADMIN";
+        protected static final String ROLE_PREFIX = "ROLE_";
+        public static final String ROLE_USER = ROLE_PREFIX + USER;
+        public static final String ROLE_ADMIN = ROLE_PREFIX + ADMIN;
+    }
+}
--- src/main/java/com/evgeniykudashov/adservice/model/domain/aggregate/user/User.java
+++ src/main/java/com/evgeniykudashov/adservice/model/domain/aggregate/user/User.java
@@ -1,21 +1,21 @@
 package com.evgeniykudashov.adservice.model.domain.aggregate.user;
 
 
-import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.AccessDetails;
-import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.PersonalDetails;
+import com.evgeniykudashov.adservice.annotation.Default;
+import com.evgeniykudashov.adservice.model.domain.Role;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
-
-@NoArgsConstructor(onConstructor = @__({@Deprecated}))
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@AllArgsConstructor
-
+@NoArgsConstructor
+@Getter
+@Setter
+//
+@SecondaryTable(name = "user_details",
+        pkJoinColumns = @PrimaryKeyJoinColumn(name = "user_id"))
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
@@ -24,25 +24,44 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     @EqualsAndHashCode.Include
-    @Getter
     private long id;
 
-    @Getter
-    private PersonalDetails personalDetails;
+    String firstName;
+    String lastName;
+    LocalDate birthdate;
+    String email;
+    @Column(table = "user_details", unique = true)
+    String username;
+    @Column(table = "user_details")
+    String password;
+    @Column(table = "user_details")
+    @Enumerated(value = EnumType.STRING)
+    Role role;
 
-    @Getter
-    private AccessDetails accessDetails;
-
-    public User(PersonalDetails personalDetails, AccessDetails accessDetails) {
-        this.personalDetails = personalDetails;
-        this.accessDetails = accessDetails;
+    public User(@NonNull long id,
+                @NonNull String firstName,
+                @NonNull String lastName,
+                @NonNull LocalDate birthdate,
+                @NonNull String email,
+                @NonNull String username,
+                @NonNull String password,
+                @NonNull Role role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
-    public void updatePersonalDetails(PersonalDetails personalDetails) {
-        this.personalDetails = personalDetails;
-    }
-
-    public void updateAccessDetails(AccessDetails accessDetails) {
-        this.accessDetails = accessDetails;
+    @Default
+    public User(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 }
+
+//
