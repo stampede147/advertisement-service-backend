ce18f32bedcc4cec6414e3b696fd1288dedff593
Eugene <stampede147@gmail.com>
2023-07-01 20:20:46 +0300

    changed jpa script

--- src/main/java/com/evgeniykudashov/adservice/repository/UserRepository.java
+++ src/main/java/com/evgeniykudashov/adservice/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.evgeniykudashov.adservice.repository;
 
-import com.evgeniykudashov.adservice.model.domain.aggregate.user.User;
-import com.evgeniykudashov.adservice.model.domain.shared.security.Username;
+import com.evgeniykudashov.adservice.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,7 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
-    @Query("from User u where u.accessDetails.username = :u")
-    public Optional<User> findByUsername(Username u);
+    @Query("from User u where u.username = :username")
+    Optional<User> findByUsername(String username);
 
 }
