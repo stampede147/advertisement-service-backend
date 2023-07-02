74d27988348dd90a852b0e01247a4e4941da1f43
Eugene <stampede147@gmail.com>
2023-07-02 19:09:16 +0300

    deleted dto mapper and POST, PUT, GET (by id) methods

--- src/main/java/com/evgeniykudashov/adservice/controller/rest/UserAdvertisementController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/UserAdvertisementController.java
@@ -1,22 +1,17 @@
 package com.evgeniykudashov.adservice.controller.rest;
 
 
-import com.evgeniykudashov.adservice.mapper.AdvertisementMapper;
-import com.evgeniykudashov.adservice.mapper.dto.advertisement.CreateAdvertisementRequestDto;
-import com.evgeniykudashov.adservice.mapper.dto.advertisement.UpdateAdvertisementRequestDto;
-import com.evgeniykudashov.adservice.model.domain.aggregate.advertisement.Advertisement;
+import com.evgeniykudashov.adservice.mapper.dto.PageDto;
 import com.evgeniykudashov.adservice.service.AdvertisementService;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users/{userId}/advertisements")
@@ -24,46 +19,10 @@ import java.util.Map;
 public class UserAdvertisementController {
 
     private AdvertisementService advertisementService;
-    private AdvertisementMapper dtoEntityMapper;
-
-    @PostMapping()
-    public ResponseEntity<Void> onCreate(@RequestBody CreateAdvertisementRequestDto requestDto) {
-        long resourceId = advertisementService.create(dtoEntityMapper.toAdvertisement(requestDto));
-        return ResponseEntity
-                .created(ServletUriComponentsBuilder.fromCurrentRequestUri()
-                        .path("/{id}")
-                        .build(resourceId))
-                .build();
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> onPut(@RequestBody UpdateAdvertisementRequestDto requestDto, @PathVariable Long id) {
-        Advertisement advertisement = dtoEntityMapper.toAdvertisement(requestDto);
-        advertisementService.put(advertisement, id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @ExceptionHandler(MismatchedInputException.class)
-    public ResponseEntity<?> handlePatchMethodExceptions(MismatchedInputException e) {
-        return ResponseEntity.badRequest().body(Map.of("message", "unknown properties found"));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> onDelete(@PathVariable Long id) {
-        advertisementService.remove(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> onGetById(@PathVariable Long id) {
-        return ResponseEntity.ok(dtoEntityMapper.toAdvertisementResponseDto(advertisementService.findById(id)));
-    }
 
     @GetMapping()
-    public ResponseEntity<?> onGetAll(@PathVariable Long userId,
-                                      @PageableDefault Pageable pageable) {
-        Page<Advertisement> allByUserId = advertisementService.findAllByUserId(userId, pageable);
-        return ResponseEntity.ok(
-                dtoEntityMapper.toPageDto(allByUserId));
+    public ResponseEntity<PageDto<?>> onGetAll(@PathVariable Long userId,
+                                               @PageableDefault Pageable pageable) {
+        return ResponseEntity.ok(advertisementService.findAllByUserId(userId, pageable));
     }
 }
--- src/main/java/com/evgeniykudashov/adservice/controller/rest/UserChatController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/UserChatController.java
@@ -1,16 +1,16 @@
 package com.evgeniykudashov.adservice.controller.rest;
 
 
-import com.evgeniykudashov.adservice.mapper.ChatMapper;
-import com.evgeniykudashov.adservice.mapper.dto.chat.CreateChatRequestDto;
 import com.evgeniykudashov.adservice.service.ChatService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @AllArgsConstructor(onConstructor_ = @Autowired)
@@ -18,29 +18,11 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestMapping("/users/{userId}/chats")
 public class UserChatController {
 
-
     private ChatService chatService;
-    private ChatMapper dtoEntityMapper;
-
-    @PostMapping
-    public ResponseEntity<Void> createChat(@RequestBody CreateChatRequestDto chat) {
-        return ResponseEntity
-                .created(ServletUriComponentsBuilder.fromCurrentRequestUri()
-                        .path("/{id}")
-                        .build(chatService.create(dtoEntityMapper.ToChat(chat))))
-                .build();
-    }
-
-    @DeleteMapping("/{chatId}")
-    public ResponseEntity<Void> deleteChat(@PathVariable long chatId) {
-        chatService.remove(chatId);
-        return ResponseEntity.noContent().build();
-    }
 
     @GetMapping()
     public ResponseEntity<?> findAll(@PathVariable long userId,
                                      @PageableDefault Pageable pageable) {
-
-        return ResponseEntity.ok(dtoEntityMapper.toPageDto(chatService.findAllByUserId(userId, pageable)));
+        return ResponseEntity.ok(chatService.findAllByUserId(userId, pageable));
     }
 }
