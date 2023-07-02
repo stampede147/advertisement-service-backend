39422703d10a2adcc5a739dee75c450e7d2c9ac6
Eugene <stampede147@gmail.com>
2023-07-02 14:31:39 +0300

    marked field algorithm final

--- src/main/java/com/evgeniykudashov/adservice/security/jwt/tokenfactory/JwtTokenFactory.java
+++ src/main/java/com/evgeniykudashov/adservice/security/jwt/tokenfactory/JwtTokenFactory.java
@@ -3,8 +3,8 @@ package com.evgeniykudashov.adservice.security.jwt.tokenfactory;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.evgeniykudashov.adservice.model.domain.aggregate.user.valueobject.Role;
-import com.evgeniykudashov.adservice.security.jwt.JwtAuthenticationToken;
+import com.evgeniykudashov.adservice.model.user.Role;
+import com.evgeniykudashov.adservice.security.jwt.OAuthAuthenticationToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -20,7 +20,7 @@ public class JwtTokenFactory implements OAuthTokenFactory {
 
     protected int tokenLifeMillis;
 
-    private Algorithm algorithm;
+    private final Algorithm algorithm;
 
     public JwtTokenFactory(@Value("${application.security.jwt.secret}") String jwtSecret,
                            @Value("${application.security.jwt.lifeMillis}") int tokenLifeMillis) {
@@ -56,4 +56,14 @@ public class JwtTokenFactory implements OAuthTokenFactory {
                 verify(token);
     }
 
+    protected static class JwtAuthenticationToken extends OAuthAuthenticationToken {
+
+        public JwtAuthenticationToken(Collection<? extends GrantedAuthority> authorities,
+                                      String username,
+                                      Instant expiresAt,
+                                      Instant issuedAt) {
+            super(authorities, username, expiresAt, issuedAt);
+        }
+
+    }
 }
