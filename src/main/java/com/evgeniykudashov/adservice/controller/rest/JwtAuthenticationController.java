ec33debb326550cf14b4a9213a8aed2a9338bddc
Eugene <stampede147@gmail.com>
2023-07-03 18:28:02 +0300

    renamed from AuthenticationController.java

--- src/main/java/com/evgeniykudashov/adservice/controller/rest/JwtAuthenticationController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/JwtAuthenticationController.java
@@ -3,6 +3,7 @@ package com.evgeniykudashov.adservice.controller.rest;
 
 import com.evgeniykudashov.adservice.mapper.dto.UsernamePasswordDto;
 import com.evgeniykudashov.adservice.service.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,13 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/authentications")
 @AllArgsConstructor(onConstructor_ = @Autowired)
-public class AuthenticationController {
+public class JwtAuthenticationController {
 
     public static final String ACCESS_TOKEN = "access_token";
     protected AuthenticationService authenticationService;
 
     @PostMapping("/jwt")
-    public ResponseEntity<Void> createJwtAuthentication(@RequestBody UsernamePasswordDto dto) {
+    public ResponseEntity<Void> createJwtAuthentication(@RequestBody @Valid UsernamePasswordDto dto) {
         return ResponseEntity.noContent()
                 .headers(headers -> headers.add(HttpHeaders.SET_COOKIE,
                         ResponseCookie.from(ACCESS_TOKEN, authenticationService.generateJwtToken(dto))
