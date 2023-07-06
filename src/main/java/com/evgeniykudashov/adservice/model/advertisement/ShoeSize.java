f33edd939a48761eac2ce40a500ec6ae0331a53e
Eugene <stampede147@gmail.com>
2023-07-06 20:14:44 +0300

    added new entities

---
+++ src/main/java/com/evgeniykudashov/adservice/model/advertisement/ShoeSize.java
@@ -0,0 +1,24 @@
+package com.evgeniykudashov.adservice.model.advertisement;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "shoe_sizes")
+public class ShoeSize {
+
+    @Id
+    int size;
+
+}
