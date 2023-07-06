bfa06a8b49d2e22953f39ed26856e020765f7bde
Eugene <stampede147@gmail.com>
2023-07-06 18:43:50 +0300

    added attribute price

--- src/main/java/com/evgeniykudashov/adservice/model/advertisement/Advertisement.java
+++ src/main/java/com/evgeniykudashov/adservice/model/advertisement/Advertisement.java
@@ -3,20 +3,26 @@ package com.evgeniykudashov.adservice.model.advertisement;
 import com.evgeniykudashov.adservice.model.user.User;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.Immutable;
 
 import java.io.Serializable;
 
 @NoArgsConstructor()
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@SuperBuilder
+
 @SecondaryTable(name = "addresses",
         pkJoinColumns = @PrimaryKeyJoinColumn(name = "advertisement_id"),
         uniqueConstraints = @UniqueConstraint(columnNames = "advertisement_id"))
-@Getter
-@Setter
+
 @Entity
 @Table(name = "advertisements")
+@Inheritance(strategy = InheritanceType.JOINED)
 @BatchSize(size = 10)
 public class Advertisement implements Serializable {
 
@@ -34,6 +40,7 @@ public class Advertisement implements Serializable {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", updatable = false)
     @Immutable
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private User owner;
 
     @Enumerated(value = EnumType.ORDINAL)
@@ -41,6 +48,8 @@ public class Advertisement implements Serializable {
 
     @Column(table = "addresses")
     private Address address;
+
+    private long price;
 
     public Advertisement(long id,
                          @NonNull String title,
