ec9a4ea21522f3ec2fe1e69e780f51bd01eaece4
Eugene <stampede147@gmail.com>
2023-07-09 21:19:22 +0300

    added service, mapper, controller for ThingColor.java model

---
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/ThingColorController.java
@@ -0,0 +1,39 @@
+package com.evgeniykudashov.adservice.controller.rest;
+
+import com.evgeniykudashov.adservice.model.advertisement.ThingColor;
+import com.evgeniykudashov.adservice.service.ThingColorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@RequestMapping("/thingcolors")
+public class ThingColorController {
+
+    private final ThingColorService thingColorService;
+
+    @PostMapping
+    public ResponseEntity<?> createThingColor(@RequestBody ThingColor color) {
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("{id}")
+                        .build(thingColorService.createThingColor(color)))
+                .build();
+    }
+
+    @DeleteMapping(params = "color")
+    public void deleteThingColor(@RequestParam String color) {
+        this.thingColorService.deleteThingColorByColor(color);
+    }
+
+    @GetMapping
+    public List<ThingColor> getShoeSizes() {
+        return thingColorService.getThingColors();
+    }
+
+
+}
---
+++ src/main/java/com/evgeniykudashov/adservice/dto/ThingColorDto.java
@@ -0,0 +1,13 @@
+package com.evgeniykudashov.adservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ThingColorDto {
+
+    private String color;
+}
