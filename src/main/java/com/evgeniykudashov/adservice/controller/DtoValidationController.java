03b7de4312dffe665a30628018a2009959808516
Eugene <stampede147@gmail.com>
2023-07-11 23:05:30 +0300

    provded new exception handling for exception: InvalidIdException

--- src/main/java/com/evgeniykudashov/adservice/controller/DtoValidationController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/DtoValidationController.java
@@ -3,23 +3,31 @@ package com.evgeniykudashov.adservice.controller;
 
 import com.evgeniykudashov.adservice.dto.ErrorDto;
 import com.evgeniykudashov.adservice.dto.response.ErrorResponseDto;
+import com.evgeniykudashov.adservice.exception.InvalidIdException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class DtoValidationController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ErrorResponseDto processRSR303Validation(final MethodArgumentNotValidException e) {
         return new ErrorResponseDto(e.getBindingResult().getFieldErrors().stream()
                 .map(fieldError -> ErrorDto.of(fieldError.getField(), fieldError.getDefaultMessage()))
                 .collect(Collectors.toSet()));
+    }
+
+    @ExceptionHandler(InvalidIdException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<?, ?> invalidId(InvalidIdException e) {
+        return Map.of("error: ", e.getMessage());
     }
 
 }
