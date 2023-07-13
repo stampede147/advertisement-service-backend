f7c7416a30b2e905e94e8109ab868667d765e30f
Eugene <stampede147@gmail.com>
2023-07-13 22:09:29 +0300

    renamed field from message to description

--- src/main/java/com/evgeniykudashov/adservice/dto/ErrorDto.java
+++ src/main/java/com/evgeniykudashov/adservice/dto/ErrorDto.java
@@ -7,11 +7,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ErrorDto {
     private String field;
-    private String message;
+    private String description;
 
     private ErrorDto(String field, String message) {
         this.field = field;
-        this.message = message;
+        this.description = message;
     }
 
     public static ErrorDto of(String field, String message) {
