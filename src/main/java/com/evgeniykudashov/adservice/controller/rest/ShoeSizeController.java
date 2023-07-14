1fd7b7ce3726310bb07ee1e152b9cea6cecace29
Eugene <stampede147@gmail.com>
2023-07-14 21:49:40 +0300

    Added swagger operations

--- src/main/java/com/evgeniykudashov/adservice/controller/rest/ShoeSizeController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/ShoeSizeController.java
@@ -3,6 +3,8 @@ package com.evgeniykudashov.adservice.controller.rest;
 import com.evgeniykudashov.adservice.dto.ShoeSizeDto;
 import com.evgeniykudashov.adservice.model.advertisement.ShoeSize;
 import com.evgeniykudashov.adservice.service.ShoeSizeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
+
+@Tag(name = "Shoesize", description = "provided API about shoe sizes")
+
 
 @RestController
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -21,6 +26,7 @@ public class ShoeSizeController {
 
     private final ShoeSizeService shoeSizeService;
 
+    @Operation(description = "creates new shoe size")
     @PostMapping
     public ResponseEntity<?> createShoeSize(ShoeSizeDto dto) {
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
@@ -29,11 +35,13 @@ public class ShoeSizeController {
                 .build();
     }
 
+    @Operation(description = "deletes shoe size")
     @DeleteMapping("/{size}")
     public void deleteShoeSize(@PathVariable int size) {
         deleteShoeSize(size);
     }
 
+    @Operation(description = "returns an array of shoe sizes")
     @GetMapping
     public List<ShoeSize> getShoeSizes() {
         return shoeSizeService.getShoeSizes();
--- src/main/java/com/evgeniykudashov/adservice/controller/rest/ThingColorController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/ThingColorController.java
@@ -2,6 +2,8 @@ package com.evgeniykudashov.adservice.controller.rest;
 
 import com.evgeniykudashov.adservice.model.advertisement.ThingColor;
 import com.evgeniykudashov.adservice.service.ThingColorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
+
+@Tag(name = "ThingColor", description = "provided API about thing colors")
 
 @RestController
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -20,6 +24,7 @@ public class ThingColorController {
 
     private final ThingColorService thingColorService;
 
+    @Operation(description = "creates the color")
     @PostMapping
     public ResponseEntity<?> createThingColor(@RequestBody ThingColor color) {
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
@@ -28,11 +33,13 @@ public class ThingColorController {
                 .build();
     }
 
+    @Operation(description = "deletes the color")
     @DeleteMapping(params = "color")
     public void deleteThingColor(@RequestParam String color) {
         this.thingColorService.deleteThingColorByColor(color);
     }
 
+    @Operation(description = "returns an array of colors")
     @GetMapping
     public List<ThingColor> getShoeSizes() {
         return thingColorService.getThingColors();
