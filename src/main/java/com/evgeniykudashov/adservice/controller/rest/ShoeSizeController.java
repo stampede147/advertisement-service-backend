b228b94d937d6e3cabdb6de58e6e6e92c679d633
Eugene <stampede147@gmail.com>
2023-07-15 19:47:15 +0300

    added new descriptions for operations

--- src/main/java/com/evgeniykudashov/adservice/controller/rest/ShoeSizeController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/ShoeSizeController.java
@@ -4,9 +4,12 @@ import com.evgeniykudashov.adservice.dto.ShoeSizeDto;
 import com.evgeniykudashov.adservice.model.advertisement.ShoeSize;
 import com.evgeniykudashov.adservice.service.ShoeSizeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +30,9 @@ public class ShoeSizeController {
     private final ShoeSizeService shoeSizeService;
 
     @Operation(description = "creates new shoe size")
+    @ApiResponse(responseCode = "201",
+            description = "created successfully",
+            headers = @Header(name = HttpHeaders.LOCATION, description = "The location of created resource"))
     @PostMapping
     public ResponseEntity<?> createShoeSize(ShoeSizeDto dto) {
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
@@ -35,13 +41,18 @@ public class ShoeSizeController {
                 .build();
     }
 
-    @Operation(description = "deletes shoe size")
-    @DeleteMapping("/{size}")
-    public void deleteShoeSize(@PathVariable int size) {
-        deleteShoeSize(size);
+    @Operation(description = "deletes shoe size by its ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "(OK) Deleted successfully"),
+                    @ApiResponse(responseCode = "404", description = "(NOT FOUND) Not found such shoe size by ID")
+            })
+    @DeleteMapping("/{sizeId}")
+    public void deleteShoeSize(@PathVariable int sizeId) {
+        shoeSizeService.delete(sizeId);
     }
 
     @Operation(description = "returns an array of shoe sizes")
+    @ApiResponse(responseCode = "200", description = "(OK) returns shoe sizes")
     @GetMapping
     public List<ShoeSize> getShoeSizes() {
         return shoeSizeService.getShoeSizes();
--- src/main/java/com/evgeniykudashov/adservice/controller/rest/ThingColorController.java
+++ src/main/java/com/evgeniykudashov/adservice/controller/rest/ThingColorController.java
@@ -3,9 +3,12 @@ package com.evgeniykudashov.adservice.controller.rest;
 import com.evgeniykudashov.adservice.model.advertisement.ThingColor;
 import com.evgeniykudashov.adservice.service.ThingColorService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +28,9 @@ public class ThingColorController {
     private final ThingColorService thingColorService;
 
     @Operation(description = "creates the color")
+    @ApiResponse(responseCode = "201",
+            description = "created successfully",
+            headers = @Header(name = HttpHeaders.LOCATION, description = "The location of created resource"))
     @PostMapping
     public ResponseEntity<?> createThingColor(@RequestBody ThingColor color) {
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
@@ -33,13 +39,18 @@ public class ThingColorController {
                 .build();
     }
 
-    @Operation(description = "deletes the color")
-    @DeleteMapping(params = "color")
-    public void deleteThingColor(@RequestParam String color) {
-        this.thingColorService.deleteThingColorByColor(color);
+    @Operation(description = "deletes the color",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "(OK) Deleted successfully"),
+                    @ApiResponse(responseCode = "404", description = "(NOT FOUND) Not found such color by ID")
+            })
+    @DeleteMapping(params = "colorId")
+    public void deleteThingColor(@RequestParam String colorId) {
+        this.thingColorService.deleteThingColorByColor(colorId);
     }
 
     @Operation(description = "returns an array of colors")
+    @ApiResponse(responseCode = "200", description = "(OK) returns colors")
     @GetMapping
     public List<ThingColor> getShoeSizes() {
         return thingColorService.getThingColors();
