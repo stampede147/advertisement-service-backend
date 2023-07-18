8747a64276dd25fad3940cc55f029f3573fd0e72
Eugene <stampede147@gmail.com>
2023-07-18 15:01:56 +0300

    added constructors

--- src/main/java/com/evgeniykudashov/adservice/exception/servicelayer/NotFoundEntityException.java
+++ src/main/java/com/evgeniykudashov/adservice/exception/servicelayer/NotFoundEntityException.java
@@ -4,6 +4,30 @@ package com.evgeniykudashov.adservice.exception.servicelayer;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(reason = "not found such entity", code = HttpStatus.NOT_FOUND)
+import static com.evgeniykudashov.adservice.exception.servicelayer.NotFoundEntityException.NOT_FOUND_MESSAGE;
+
+@ResponseStatus(reason = NOT_FOUND_MESSAGE, code = HttpStatus.NOT_FOUND)
 public class NotFoundEntityException extends ServiceLayerException {
+
+    protected static final String NOT_FOUND_MESSAGE = "Not found such entity";
+
+    public NotFoundEntityException() {
+        this(NOT_FOUND_MESSAGE);
+    }
+
+    public NotFoundEntityException(String message) {
+        super(message);
+    }
+
+    public NotFoundEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundEntityException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundEntityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
