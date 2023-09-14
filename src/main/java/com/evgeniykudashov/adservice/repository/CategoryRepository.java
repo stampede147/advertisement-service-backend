b0f257da7301cdbd84fcd87bfd3b0087e05d974c
Eugene <stampede147@gmail.com>
2023-09-15 00:30:50 +0300

    added new repository

--- src/main/java/com/evgeniykudashov/adservice/repository/CategoryRepository.java
+++ src/main/java/com/evgeniykudashov/adservice/repository/CategoryRepository.java
@@ -1,2 +1,7 @@
-package com.evgeniykudashov.adservice.repository;public interface CategoryRepository {
+package com.evgeniykudashov.adservice.repository;
+
+import com.evgeniykudashov.adservice.model.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
