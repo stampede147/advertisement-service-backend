91b585ad01118f0d799dc995c4721fc46c69f688
Eugene <stampede147@gmail.com>
2023-10-02 22:49:21 +0300

    added JacksonConfiguration.java

---
+++ src/main/java/com/evgeniykudashov/adservice/configuration/JacksonConfiguration.java
@@ -0,0 +1,24 @@
+package com.evgeniykudashov.adservice.configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Autowired
+    public void mapperConfiguration(ObjectMapper mapper) {
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.enable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+        mapper.registerModule(new ParameterNamesModule());
+        mapper.registerModule(new JavaTimeModule());
+    }
+}
