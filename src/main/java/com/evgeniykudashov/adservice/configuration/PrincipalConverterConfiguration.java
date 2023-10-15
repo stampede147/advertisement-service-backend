080513a085ecb6fdfe6847ca2338139ecbb49e04
Eugene <stampede147@gmail.com>
2023-10-15 14:56:08 +0300

    renamed bean name

--- src/main/java/com/evgeniykudashov/adservice/configuration/PrincipalConverterConfiguration.java
+++ src/main/java/com/evgeniykudashov/adservice/configuration/PrincipalConverterConfiguration.java
@@ -1,9 +1,7 @@
 package com.evgeniykudashov.adservice.configuration;
 
-import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.core.convert.converter.Converter;
 
 import java.security.Principal;
@@ -11,17 +9,16 @@ import java.security.Principal;
 @Configuration
 public class PrincipalConverterConfiguration {
 
-    private static class PrincipalConverter implements Converter<Principal, Long> {
+    @Bean
+    public Converter<Principal, Long> principalConverter() {
+        return new PrincipalToUserIdConverter();
+    }
+
+    private static class PrincipalToUserIdConverter implements Converter<Principal, Long> {
         @Override
         public Long convert(Principal source) {
             return Long.valueOf(source.getName());
         }
-    }
-
-    @Bean
-    @Scope
-    public Converter<Principal, Long> principalConverter() {
-        return new PrincipalConverter();
     }
 
 
