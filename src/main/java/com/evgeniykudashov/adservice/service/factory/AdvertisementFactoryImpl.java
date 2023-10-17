1c1d722c8d58695e273ed78c8f69ec005e50c360
Eugene <stampede147@gmail.com>
2023-10-17 13:25:06 +0300

    added AdvertisementFactory

---
+++ src/main/java/com/evgeniykudashov/adservice/service/factory/AdvertisementFactoryImpl.java
@@ -0,0 +1,37 @@
+package com.evgeniykudashov.adservice.service.factory;
+
+import com.evgeniykudashov.adservice.model.advertisement.Advertisement;
+import com.evgeniykudashov.adservice.model.advertisement.AdvertisementStatus;
+import com.evgeniykudashov.adservice.model.category.Category;
+import com.evgeniykudashov.adservice.model.image.ImageEntity;
+import com.evgeniykudashov.adservice.model.user.User;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class AdvertisementFactoryImpl implements AdvertisementFactory {
+
+    @Override
+    public Advertisement createAdvertisement(Supplier<Advertisement> advertisementSupplier,
+                                             Supplier<User> sellerSupplier,
+                                             Supplier<Category> categorySupplier,
+                                             Supplier<List<ImageEntity>> imagesSupplier) {
+
+        Advertisement advertisement = advertisementSupplier.get();
+
+
+        advertisement.setStatus(AdvertisementStatus.CREATED);
+
+        advertisement.setStartTime(LocalDate.now());
+
+        advertisement.setSeller(sellerSupplier.get());
+
+        advertisement.setCategory(categorySupplier.get());
+
+        advertisement.setImages(imagesSupplier.get());
+
+
+        return advertisement;
+    }
+}
