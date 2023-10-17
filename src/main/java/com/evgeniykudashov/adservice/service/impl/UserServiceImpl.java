df309b673eaf398ac494f3bbafea11f4a83101e2
Eugene <stampede147@gmail.com>
2023-10-17 13:26:01 +0300

    now used userfactory

--- src/main/java/com/evgeniykudashov/adservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/evgeniykudashov/adservice/service/impl/UserServiceImpl.java
@@ -3,27 +3,21 @@ package com.evgeniykudashov.adservice.service.impl;
 import com.evgeniykudashov.adservice.dto.request.UserRequestDto;
 import com.evgeniykudashov.adservice.dto.response.UserResponseDto;
 import com.evgeniykudashov.adservice.exception.servicelayer.NotFoundEntityException;
-import com.evgeniykudashov.adservice.mapper.ImageEntityMapper;
 import com.evgeniykudashov.adservice.mapper.UserMapper;
-import com.evgeniykudashov.adservice.model.image.ImageEntity;
 import com.evgeniykudashov.adservice.model.user.User;
-import com.evgeniykudashov.adservice.repository.ImageEntityRepository;
 import com.evgeniykudashov.adservice.repository.UserRepository;
 import com.evgeniykudashov.adservice.service.UserService;
-import com.evgeniykudashov.adservice.service.utils.UserFactory;
+import com.evgeniykudashov.adservice.service.factory.UserFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.file.Path;
 import java.security.Principal;
-import java.util.function.Supplier;
 
 
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -33,49 +27,23 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    private final ImageEntityRepository imageEntityRepository;
-
     private final UserFactory userFactory;
 
     private final UserMapper mapper;
 
-    private final PasswordEncoder encoder;
-
-    private final ImageEntityMapper imageEntityMapper;
-
     private final Converter<Principal, Long> principalConverter;
-
-    private static String getDirectoryPath(String name) {
-        return name.substring(0, 1).toUpperCase();
-    }
 
     @Transactional
     @Override
     @SneakyThrows
     public long create(UserRequestDto dto) {
-        log.trace("Started create(UserRequestDto) method");
+        log.trace("Started create(Us erRequestDto) method");
         log.debug("Provided parameter dto: {}", dto);
 
-        String stubAvatarPathLocation = getPathLocation(dto.firstName);
+        User user = userFactory.createUser(() -> mapper.toUser(dto));
 
-        Supplier<ImageEntity> getImageEntityCallback = () -> imageEntityRepository.findByLocation(stubAvatarPathLocation)
-                .orElseGet(() -> imageEntityRepository.save(new ImageEntity(null, stubAvatarPathLocation)));
-
-        User user = userFactory.createUser(dto, getImageEntityCallback);
-
-
-        return userRepository.save(user).getId();
-    }
-
-    private String getPathLocation(String name) {
-
-        final String path = "static/images/stub_avatars";
-
-        return Path.of(path)
-                .resolve(getDirectoryPath(name))
-                .resolve(String.format("%s.png", "256x256"))
-                .toUri()
-                .toString();
+        return userRepository.save(user)
+                .getId();
     }
 
 
@@ -99,7 +67,8 @@ public class UserServiceImpl implements UserService {
     }
 
     private User getUser(long userId) {
-        return userRepository.findById(userId).orElseThrow(NotFoundEntityException::new);
+        return userRepository.findById(userId)
+                .orElseThrow(NotFoundEntityException::new);
     }
 
     @Override
