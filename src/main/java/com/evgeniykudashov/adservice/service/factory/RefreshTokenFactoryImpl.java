462343ae3c2a3710760320df62eecc392d39bd90
Eugene <stampede147@gmail.com>
2023-10-25 22:06:21 +0300

    refactored

--- src/main/java/com/evgeniykudashov/adservice/service/factory/RefreshTokenFactoryImpl.java
+++ src/main/java/com/evgeniykudashov/adservice/service/factory/RefreshTokenFactoryImpl.java
@@ -2,6 +2,7 @@ package com.evgeniykudashov.adservice.service.factory;
 
 import com.evgeniykudashov.adservice.model.RefreshToken;
 import com.evgeniykudashov.adservice.model.user.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -12,7 +13,12 @@ import java.util.function.Supplier;
 @Component
 public class RefreshTokenFactoryImpl implements RefreshTokenFactory {
 
-    private static final long REFRESH_TOKEN_LIFE_MILLIS = 7 * 24 * 60 * 1000; // 7 days
+    private final long refreshTokenLifeMillis;
+
+    public RefreshTokenFactoryImpl(@Value("${application.security.jwt.refreshLifeMillis}")
+                                   long refreshTokenLifeMillis) {
+        this.refreshTokenLifeMillis = refreshTokenLifeMillis;
+    }
 
     @Override
     public RefreshToken create(Supplier<User> userSupplier) {
@@ -24,7 +30,7 @@ public class RefreshTokenFactoryImpl implements RefreshTokenFactory {
 
         LocalDateTime now = LocalDateTime.now();
         refreshToken.setCreatedAt(now);
-        refreshToken.setExpiresAt(now.plus(REFRESH_TOKEN_LIFE_MILLIS, ChronoUnit.MILLIS));
+        refreshToken.setExpiresAt(now.plus(refreshTokenLifeMillis, ChronoUnit.MILLIS));
 
         return refreshToken;
     }
