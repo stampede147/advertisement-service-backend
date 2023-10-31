63e4a7310f07daf050749092488f462a5198e091
Evgeniy Kudashov <kudashovea@gmail.com>
2023-10-31 21:33:53 +0300

    added findReviewsByBuyerId(long, Pageable) method

--- src/main/java/com/evgeniykudashov/adservice/repository/ReviewRepository.java
+++ src/main/java/com/evgeniykudashov/adservice/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.evgeniykudashov.adservice.repository;
 
 import com.evgeniykudashov.adservice.model.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findReviewsByBuyerId(long buyerId, Pageable pageable);
+
+    Page<Review> findReviewsByRecipientId(long recipientId, Pageable pageable);
 }
