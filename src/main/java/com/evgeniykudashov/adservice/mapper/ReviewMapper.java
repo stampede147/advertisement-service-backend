2ad7fc6d0a38a823d2828a18c5affc693d856409
Evgeniy Kudashov <kudashovea@gmail.com>
2023-10-31 20:46:10 +0300

    added new mapper method toReview(ReviewRequestDto, long, LocalDate, AdvertisementRepository, UserRepository)

--- src/main/java/com/evgeniykudashov/adservice/mapper/ReviewMapper.java
+++ src/main/java/com/evgeniykudashov/adservice/mapper/ReviewMapper.java
@@ -1,16 +1,30 @@
 package com.evgeniykudashov.adservice.mapper;
 
+import com.evgeniykudashov.adservice.dto.request.ReviewRequestDto;
 import com.evgeniykudashov.adservice.dto.response.PageDto;
 import com.evgeniykudashov.adservice.dto.response.ReviewResponseDto;
 import com.evgeniykudashov.adservice.model.Review;
+import com.evgeniykudashov.adservice.repository.AdvertisementRepository;
+import com.evgeniykudashov.adservice.repository.UserRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
 
 
 @Mapper(componentModel = "spring", uses = AdvertisementMapper.class)
 public abstract class ReviewMapper {
 
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "advertisement", expression = "java(advertisementRepository.findById(reviewDto.getAdvertisementId()).orElseThrow(com.evgeniykudashov.adservice.exception.servicelayer.NotFoundEntityException::new))")
+    @Mapping(target = "buyer", expression = "java(userRepository.getReferenceById(buyerUserId))")
+    public abstract Review toReview(ReviewRequestDto reviewDto,
+                                    long buyerUserId,
+                                    LocalDate createdAt,
+                                    AdvertisementRepository advertisementRepository,
+                                    UserRepository userRepository);
 
     @Mapping(target = "rate", expression = "java(review.getRate().ordinal())")
     public abstract ReviewResponseDto toReviewResponseDto(Review review);
