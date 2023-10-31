37fa8ad57a7c02f5f934f513759f3861698362e3
Evgeniy Kudashov <kudashovea@gmail.com>
2023-10-31 20:45:22 +0300

    provided new method getValue() with @JsonValue annotation

--- src/main/java/com/evgeniykudashov/adservice/model/Rate.java
+++ src/main/java/com/evgeniykudashov/adservice/model/Rate.java
@@ -1,6 +1,13 @@
 package com.evgeniykudashov.adservice.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Rate {
 
-    ZERO, ONE, TWO, THREE, FOUR, FIVE
+    ZERO, ONE, TWO, THREE, FOUR, FIVE;
+
+    @JsonValue
+    public int getValue() {
+        return this.ordinal();
+    }
 }
