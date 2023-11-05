e884d87c7c3d2138d311406a51088cf751453c59
Evgeniy Kudashov <kudashovea@gmail.com>
2023-11-05 22:23:17 +0300

    added new factory method

--- src/main/java/com/evgeniykudashov/adservice/service/factory/UserFactory.java
+++ src/main/java/com/evgeniykudashov/adservice/service/factory/UserFactory.java
@@ -1,11 +1,16 @@
 package com.evgeniykudashov.adservice.service.factory;
 
+import com.evgeniykudashov.adservice.model.image.ImageEntity;
+import com.evgeniykudashov.adservice.model.user.Role;
 import com.evgeniykudashov.adservice.model.user.User;
 
 import java.util.function.Supplier;
 
 public interface UserFactory {
 
+    User createUser(Supplier<User> userSupplier, String rawPassword, Role role, ImageEntity image);
 
-    User createUser(Supplier<User> userSupplier);
+    default User createUser(String rawPassword) {
+        return createUser(User::new, rawPassword, Role.USER, null);
+    }
 }
