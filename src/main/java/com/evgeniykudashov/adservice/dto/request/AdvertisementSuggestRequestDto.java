34e031ccee1aa07056ab27f1c52e65f4a6d27dd3
Evgeniy Kudashov <kudashovea@gmail.com>
2023-11-26 19:48:22 +0300

    new dtos

---
+++ src/main/java/com/evgeniykudashov/adservice/dto/request/AdvertisementSuggestRequestDto.java
@@ -0,0 +1,9 @@
+package com.evgeniykudashov.adservice.dto.request;
+
+import lombok.Data;
+
+@Data
+public class AdvertisementSuggestRequestDto {
+
+    public String query;
+}
---
+++ src/main/java/com/evgeniykudashov/adservice/dto/response/AdvertisementSuggestResponseDto.java
@@ -0,0 +1,18 @@
+package com.evgeniykudashov.adservice.dto.response;
+
+import com.evgeniykudashov.adservice.service.QuerySuggestStrategy;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class AdvertisementSuggestResponseDto {
+
+    @JsonInclude
+    List<QuerySuggestStrategy.Suggestion> results;
+
+
+}
