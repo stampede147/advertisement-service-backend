6edc20a03fe01a8c059a38e30a0b64e104589efc
Evgeniy Kudashov <kudashovea@gmail.com>
2024-01-13 18:31:24 +0300

    added support to registering subtypes of Advertisement entity

--- src/main/java/com/evgeniykudashov/adservice/configuration/JacksonConfiguration.java
+++ src/main/java/com/evgeniykudashov/adservice/configuration/JacksonConfiguration.java
@@ -1,12 +1,15 @@
 package com.evgeniykudashov.adservice.configuration;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class JacksonConfiguration {
@@ -20,5 +23,16 @@ public class JacksonConfiguration {
         mapper.enable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
         mapper.registerModule(new ParameterNamesModule());
         mapper.registerModule(new JavaTimeModule());
+
+
     }
+
+    @Autowired
+    public void registerJsonTypeNameAnnotationClassInheritors(ObjectMapper mapper) {
+
+        List<Class<?>> classes = ClassUtil.findClassesWithAnnotation("com.evgeniykudashov.adservice.dto.request", JsonTypeName.class);
+        mapper.registerSubtypes(classes);
+    }
+
+
 }
